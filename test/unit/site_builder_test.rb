7ad042f83176aa5f8cafbe32157f187a99bf0bb4
Trotter Cashion <cashion@gmail.com>
2010-05-28 23:07:28 -0400

    Test that we're behaving correctly in respect to hashes.

--- test/unit/site_builder_test.rb
+++ test/unit/site_builder_test.rb
@@ -20,4 +20,25 @@ class SiteBuilderTest < ActiveSupport::TestCase
     assert_equal 2, @user.paths.size
     assert @user.paths.detect { |p| p.path == DropboxData.ls_no_dirs.first.path }
   end
+
+  test "should not bother with subpaths if root hash matches" do
+    @user.paths.expects(:find_by_path).with("/").
+      returns Path.new_from_info(DropboxData.root_info)
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).never
+    @site_builder.update
+  end
+
+  test "should both with subpaths if root hash does not match" do
+    info = DropboxData.root_info
+    info.hash = "DIFFERENT"
+    @user.paths.stubs(:find_by_path).returns nil
+    @user.paths.expects(:find_by_path).with("/").returns Path.new_from_info(info)
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).with("/").returns DropboxData.ls_no_dirs
+    @site_builder.update
+
+    # We already created the root, so we're only making the new one
+    assert_equal 1, @user.paths.size
+  end
 end
