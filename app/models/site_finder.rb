12016da8954933c38490f6f350fc49d23bdd4e16
Trotter Cashion <cashion@gmail.com>
2010-05-31 16:20:23 -0400

    Split out concept of finding new sites from building sites.

---
+++ app/models/site_finder.rb
@@ -0,0 +1,59 @@
+class SiteFinder
+  def initialize(user)
+    @user = user
+    @updated_websites = []
+    @updated_paths = []
+  end
+
+  def update
+    find_websites
+    save_paths
+    update_websites
+  end
+
+  def find_websites
+    get_or_create_root(session.info("/"))
+  end
+
+  def save_paths
+    @updated_paths.each do |path|
+      path.save
+    end
+  end
+
+  def update_websites
+    @updated_websites.each do |site|
+      site.save
+      SiteBuilder.new(@user, session, site).update
+    end
+  end
+
+  def get_or_create_root(info)
+    with_path_for(info) do |path|
+      session.ls(path.path).each do |info|
+        update_website(info) if info.directory?
+      end
+    end
+  end
+
+  def update_website(info)
+    with_path_for(info) do |path|
+      path.create_website
+      @updated_websites << path.website
+    end
+  end
+
+  def with_path_for(info)
+    path = @user.paths.find_by_path(info.path)
+    path ||= @user.paths.build(:user => @user)
+    if path.last_hash != info.hash
+      @updated_paths << path
+      path.take_attributes_from_info(info)
+      yield path
+    end
+  end
+
+  def session
+    @session ||= Dropbox::Session.deserialize(@user.session)
+  end
+end
---
+++ test/unit/site_finder_test.rb
@@ -0,0 +1,85 @@
+require 'test_helper'
+
+class SitefinderTest < ActiveSupport::TestCase
+  setup do
+    @user = User.new(:session => "blah")
+    @site_finder = SiteFinder.new(@user)
+    mock_site_builder = mock('site_builder')
+    mock_site_builder.stubs(:update)
+    SiteBuilder.stubs(:new).returns mock_site_builder
+    Website.any_instance.stubs(:save)
+    Path.any_instance.stubs(:save)
+  end
+
+  test "should create the root path if it does not exist" do
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @site_finder.update
+    assert_equal 1, @user.paths.size
+
+    root_path = @user.paths.detect { |p| p.path == "/" }
+    assert root_path
+    assert_equal @user, root_path.user
+  end
+
+  test "should not create paths for anything in root that's not a directory" do
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).with("/").returns DropboxData.ls_no_dirs
+    @site_finder.update
+
+    assert_equal 1, @user.paths.size
+    assert !@user.paths.detect { |p| p.path == DropboxData.ls_no_dirs.first.path }
+  end
+
+  test "should create website for directories directly below root" do
+    contents = DropboxData.ls_only_dirs
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).with("/").returns contents
+
+    @site_finder.update
+    assert_equal 2, @user.paths.size
+
+    website_path = @user.paths.detect { |p| p.path == contents.first.path }
+    assert website_path
+    assert_equal 1, @user.websites.size
+    assert_equal website_path, @user.websites.first.path
+  end
+
+  test "should update each website" do
+    Website.any_instance.expects(:save)
+    mock_site_builder = mock("site_builder")
+    mock_site_builder.expects(:update)
+    SiteBuilder.expects(:new).with(@user, @mock_session, instance_of(Website)).
+      returns mock_site_builder
+
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).with("/").returns DropboxData.ls_only_dirs
+
+    @site_finder.update
+  end
+
+  test "should not bother with websites if root hash matches" do
+    @user.paths.expects(:find_by_path).with("/").
+      returns Path.new_from_info(DropboxData.root_info)
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).never
+    @site_finder.update
+  end
+
+  test "should bother with subpaths if root hash does not match" do
+    info = DropboxData.root_info
+    info.hash = "DIFFERENT"
+    root_path = Path.new_from_info(info)
+    root_path.user = @user
+    @user.paths.stubs(:find_by_path).returns nil
+    @user.paths.expects(:find_by_path).with("/").returns root_path
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @mock_session.expects(:ls).with("/").returns []
+    @site_finder.update
+  end
+
+  test "should save paths" do
+    Path.any_instance.expects(:save)
+    @mock_session.expects(:info).with("/").returns DropboxData.root_info
+    @site_finder.update
+  end
+end
